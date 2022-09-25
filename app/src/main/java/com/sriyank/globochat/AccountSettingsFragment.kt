dbdecd105d7e6e8cfe55fa3e4d1a0b2090e46e1b
Crucialjun <nich.otieno@gmail.com>
2022-09-25 17:04:02 +0300

    pereference

--- app/src/main/java/com/sriyank/globochat/AccountSettingsFragment.kt
+++ app/src/main/java/com/sriyank/globochat/AccountSettingsFragment.kt
@@ -5,15 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.preference.PreferenceFragmentCompat
 
 
-class AccountSettingsFragment : Fragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-
-        return inflater.inflate(R.layout.fragment_account_settings, container, false)
+class AccountSettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.account_settings,rootKey)
     }
+
 }
